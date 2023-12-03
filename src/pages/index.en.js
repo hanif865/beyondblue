11270c306c4d3bf331b5f4086e650e33d7ffa2f3
hanif865 <99798066+hanif865@users.noreply.github.com>
2023-12-03 17:50:31 +0600

    Update index.en.js

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -66,28 +66,6 @@ const Index = props => {
             </p>
           </div>
         </div>
-
-            <div className="w-full">
-              <h2 className="text-3xl">What is Gottman Method Couples Therapy?</h2>
-              <p>
-                Gottman Method Couple Therapy is a structured, goal-oriented and
-                evidence-based therapy. Intervention strategies are based upon
-                Dr.John Gottman’s three decades of scientific research with over
-                3,000 couples. This research shows us what actually works to help
-                couples achieve a long-term healthy relationship.
-                <strong>Some skills that you and your spouse or partner will learn
-                  include:</strong>
-                  <ul className="list-disc list-outside mx-10 my-6 text-sm">
-                    <li>Work through and resolve conflict, even around arguments
-                      that you’ve been having for years</li>
-                      <li>Develop a greater sense of understanding and appreciation for
-                        your partner</li>
-                        <li>Increase your fondness and love for each other</li>
-                        <li>Build soothing and coping strategies to help you and your
-                          partner through the inevitable, tough times</li>
-                  </ul>
-              </p>
-            </div>
           <div className="container">
           <div className="xl:flex items-center py-0">
             <div className="xl:w-1/3 p-5">
@@ -115,41 +93,74 @@ const Index = props => {
             </div>
           </div>
         </div>
-                  
-            <div className="w-full">
-              <h2 className="text-3xl">Most couples wait too long…</h2>
-              <p>
-                Most couples wait too long before they seek the help that they
-                need. If you are browsing this page, chances are that it’s time for
-                you to take some action.
+                   <div className="container">
+          <div className="text-center">
+            <h2>What my clients are saying...</h2>
+          </div>
+          <div className="lg:flex items-center py-3">
+            <div className="lg:w-1/3 p-5">
+              <p className="mb-0 text-lg text-gray-900">
+                <FontAwesomeIcon
+                  icon="quote-left"
+                  className="text-gray-400 mb-2"
+                />{" "}
+                Thank you – in a big way. I’m struggling to find the words to
+                express my gratitude. Thank you for helping me find my smile
+                again and teaching me about how to pursue real happiness. These
+                last few sessions have made me feel great as I’ve realized that
+                I’ve accomplished a lot with you.{" "}
+                <FontAwesomeIcon
+                  icon="quote-right"
+                  className="text-gray-400 mb-2"
+                />
               </p>
             </div>
-            <div className="w-full">
-              <h2 className="text-3xl">Imagine how you and your partner would feel
-                if you:</h2>
-              <p>
-                <ul className="list-disc list-outside mx-10 my-6 text-sm">
-                  <li>Felt a sense of trust and ease with each other
-                  </li>
-                    <li>Had confidence and skills to talk about anything without
-                      fighting</li>
-                      <li>Enjoyed a more passionate sex life</li>
-                      <li>Knew how to support each other to be the best version of
-                        yourselves</li>
-                </ul>
-               
+            <div className="lg:w-1/3 p-5">
+              <p className="mb-0 text-lg text-gray-900">
+                <FontAwesomeIcon
+                  icon="quote-left"
+                  className="text-gray-400 mb-2"
+                />{" "}
+                Excellent therapist and person! Excellent service and really
+                really helpful session where the therapist actually shows she
+                cares.{" "}
+                <FontAwesomeIcon
+                  icon="quote-right"
+                  className="text-gray-400 mb-2"
+                />
               </p>
             </div>
-<div class="aspect-w-16 aspect-h-9 mx-auto">
-  <div class="w-full" style={{height:'400px'}}>
-    <iframe class=" w-full h-full" src="https://www.youtube.com/embed/Ap1TxCYZCHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-  </div>
-</div>
-
-
-          <div className="text-center md:text-left mt-8">
+            <div className="lg:w-1/3 p-5">
+              <p className="mb-0 text-lg text-gray-900">
+                <FontAwesomeIcon
+                  icon="quote-left"
+                  className="text-gray-400 mb-2"
+                />{" "}
+                Understanding therapist! Very understanding therapist. Took the
+                time to listen and understand the situation – Flexible about
+                timing, location (via Zoom if needed) – Offering extra or
+                follow-ups when needed.{" "}
+                <FontAwesomeIcon
+                  icon="quote-right"
+                  className="text-gray-400 mb-2"
+                />
+              </p>
+            </div>
+          </div>
+        </div>
+          <div className="container">
+          <h2 className="text-5xl text-center">
+            Book a therapy session, today.
+          </h2>
+          <p className="text-center max-w-lg mx-auto text-lg">
+            Whether you're struggling with anxiety, depression, low self-esteem,
+            or relationship troubles, I can help.
+          </p>
+          <div className="text-center">
             <BookingButton />
           </div>
+        </div>
+          <!---->        
         </div>
         <div className="w-1/3"></div>
       </div>
