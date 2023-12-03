fde863f63420229376b533cca397d720411b61a0
hanif865 <99798066+hanif865@users.noreply.github.com>
2023-12-03 16:42:25 +0600

    Update index.en.js

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -1,27 +1,27 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { graphql, useStaticQuery } from "gatsby"
-import Img from "gatsby-image"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+//import { graphql, useStaticQuery } from "gatsby"
+//import Img from "gatsby-image"
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { BookingButton } from "../components/buttons"
 
 const Index = props => {
-  const images = useStaticQuery(graphql`
-    query {
-      tumee: file(
-        relativePath: {
-          eq: "tumee-bayanbileg-english-speaking-therapist-in-tokyo-beyondbleu.jpg"
-        }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 500) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
+  // const images = useStaticQuery(graphql`
+  //   query {
+  //     tumee: file(
+  //       relativePath: {
+  //         eq: "tumee-bayanbileg-english-speaking-therapist-in-tokyo-beyondbleu.jpg"
+  //       }
+  //     ) {
+  //       childImageSharp {
+  //         fluid(maxWidth: 500) {
+  //           ...GatsbyImageSharpFluid
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <Layout {...props}>
@@ -29,159 +29,100 @@ const Index = props => {
         title="Psychotherapy for Trauma, Depression &amp; Anxiety. English speaking psychologist in Tokyo"
         description="Psychotherapy for trauma, depression, anxiety, relationship issues &amp; general struggles of modern life."
       />
-      <header className="home-header py-6 md:py-16 flex items-center">
-        <div className="container relative">
-          <div className="lg:flex items-center">
-            <div className="lg:w-2/3">
-              <div className="max-w-xl mx-auto">
-                <h1 className="text-4xl lg:text-5xl text-center lg:text-left font-light">
-                  Psychotherapy for
-                  <br />
-                  <span className="font-bold">trauma</span>,{" "}
-                  <span className="font-bold">depression</span>,{" "}
-                  <span className="font-bold">anxiety</span>, relationship
-                  issues &amp; general struggles of modern life.
-                </h1>
-              </div>
-            </div>
-            <div className="lg:w-1/3 mb-6 lg:mb-0">
-              <Img
-                fluid={images.tumee.childImageSharp.fluid}
-                className="mx-auto max-w-lg shadow-xl"
-                alt="Tumee Bayanbileg - English Speaking Therapist in Tokyo"
-              />
-            </div>
-          </div>
-        </div>
-      </header>
-
-      <section className="py-20 pt-10 text-center">
-        <div className="container">
-          <div className="max-w-3xl mx-auto">
-            <p className="text-xl mb-0">
-              Most of us reach a point in our lives when we begin to feel that
-              it is time for change, but when we recognize how difficult it is
-              to alter our existing patterns of thinking and behaviors we tend
-              to give up. This in turn leads to further avoidance, fears,
-              disappointments, and decreased motivation. Ultimately these
-              experiences can leave you feeling anxious or depressed, lead to
-              difficulties in relationships, and have you living an unhappy
-              life. Although it may feel like a dead end, in reality there are
-              many ways in which you could be helped. Psychotherapy is a process
-              through which you can explore your issues, improve insight into
-              what is holding you back, and develop efficient ways to deal with
-              your problems.
-            </p>
-          </div>
-        </div>
-      </section>
-
-      <hr className="border-gray-400 max-w-6xl mx-auto" />
-
-      <section className="py-20 text-center relative">
-        <div className="container">
-          <div className="xl:flex items-center py-0">
-            <div className="xl:w-1/3 p-5">
-              <h3 className="mb-2 text-xl">
-                <FontAwesomeIcon icon="globe-asia" className=" text-2xl" />{" "}
-                PSYCHOTHERAPY JAPAN. LLC
-              </h3>
-              <div className="uppercase">Counseling &amp; Consulting</div>
-            </div>
-            <div className="xl:w-1/3 p-5">
-              <a href="tel:03-5962-3883" className="no-underline">
-                <h3 className="mb-2 text-2xl">
-                  <FontAwesomeIcon icon="phone" /> 03-5962-3883
-                </h3>
-                <div className="uppercase">CALL NOW</div>
-              </a>
-            </div>
-            <div className="xl:w-1/3 p-5">
-              <a href="mailto:info@beyondbleu.com" className="no-underline">
-                <h3 className="mb-2 text-2xl">
-                  <FontAwesomeIcon icon="envelope" /> info@beyondbleu.com
-                </h3>
-                <div className="uppercase">E-MAIL ME</div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <hr className="border-gray-400 max-w-6xl mx-auto" />
-
-      <section className="py-32 text-center">
-        <div className="container">
-          <div className="text-center">
-            <h2>What my clients are saying...</h2>
-          </div>
-          <div className="lg:flex items-center py-3">
-            <div className="lg:w-1/3 p-5">
-              <p className="mb-0 text-lg text-gray-900">
-                <FontAwesomeIcon
-                  icon="quote-left"
-                  className="text-gray-400 mb-2"
-                />{" "}
-                Thank you – in a big way. I’m struggling to find the words to
-                express my gratitude. Thank you for helping me find my smile
-                again and teaching me about how to pursue real happiness. These
-                last few sessions have made me feel great as I’ve realized that
-                I’ve accomplished a lot with you.{" "}
-                <FontAwesomeIcon
-                  icon="quote-right"
-                  className="text-gray-400 mb-2"
-                />
+      <section className="py-6 md:py-16 bg-image bg-image-plant">
+    <div className="container">
+      <div className="lg:flex">
+        <div className="lg:w-2/3">
+            <div className="w-full">
+              <h1 className="page-title b-line">Couples Therapy</h1>
+              <h2 className="font-light text-4xl">
+                Help for partnered <strong>couples</strong>,{" "}
+                <strong>premarital counseling</strong>,{" "}
+                <strong>marriage counseling</strong>, couples divorced, or considering
+                divorce, dating coaching from Gottman
+                method trained therapist
+              </h2>
+              <p className="text-lg">
+              <ul className="list-disc list-outside mx-10 my-6 text-sm">
+                <li>Do you want to reduce conflict and improve communication?</li>
+                <li>Do you long for a way to rebuild trust and connection?</li>
+                <li>Are you looking for greater emotional or sexual intimacy?</li>
+                <li>Are you wondering whether to make it work or to get a
+                  divorce?</li>
+                  <li>Could you benefit from professional support and guidance
+                    through a difficult patch in your relationship?</li>
+              </ul>
+              <p>
+                You’re not alone! Most relationships go through challenging times.
+                In fact, most couples find themselves in trouble because they’ve
+                never learned the tools and skills necessary to make a long-term
+                relationship work. What if you didn’t have to end your relationship?
+                What if, by working on these challenges, your relationship actually
+                thrives and you feel closer than ever?
+              </p>
               </p>
             </div>
-            <div className="lg:w-1/3 p-5">
-              <p className="mb-0 text-lg text-gray-900">
-                <FontAwesomeIcon
-                  icon="quote-left"
-                  className="text-gray-400 mb-2"
-                />{" "}
-                Excellent therapist and person! Excellent service and really
-                really helpful session where the therapist actually shows she
-                cares.{" "}
-                <FontAwesomeIcon
-                  icon="quote-right"
-                  className="text-gray-400 mb-2"
-                />
-              </p>
-            </div>
-            <div className="lg:w-1/3 p-5">
-              <p className="mb-0 text-lg text-gray-900">
-                <FontAwesomeIcon
-                  icon="quote-left"
-                  className="text-gray-400 mb-2"
-                />{" "}
-                Understanding therapist! Very understanding therapist. Took the
-                time to listen and understand the situation – Flexible about
-                timing, location (via Zoom if needed) – Offering extra or
-                follow-ups when needed.{" "}
-                <FontAwesomeIcon
-                  icon="quote-right"
-                  className="text-gray-400 mb-2"
-                />
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
 
-      <section className="py-32 pt-0">
-        <div className="container">
-          <h2 className="text-5xl text-center">
-            Book a therapy session, today.
-          </h2>
-          <p className="text-center max-w-lg mx-auto text-lg">
-            Whether you're struggling with anxiety, depression, low self-esteem,
-            or relationship troubles, I can help.
-          </p>
-          <div className="text-center">
+            <div className="w-full">
+              <h2 className="text-3xl">What is Gottman Method Couples Therapy?</h2>
+              <p>
+                Gottman Method Couple Therapy is a structured, goal-oriented and
+                evidence-based therapy. Intervention strategies are based upon
+                Dr.John Gottman’s three decades of scientific research with over
+                3,000 couples. This research shows us what actually works to help
+                couples achieve a long-term healthy relationship.
+                <strong>Some skills that you and your spouse or partner will learn
+                  include:</strong>
+                  <ul className="list-disc list-outside mx-10 my-6 text-sm">
+                    <li>Work through and resolve conflict, even around arguments
+                      that you’ve been having for years</li>
+                      <li>Develop a greater sense of understanding and appreciation for
+                        your partner</li>
+                        <li>Increase your fondness and love for each other</li>
+                        <li>Build soothing and coping strategies to help you and your
+                          partner through the inevitable, tough times</li>
+                  </ul>
+              </p>
+            </div>
+            <div className="w-full">
+              <h2 className="text-3xl">Most couples wait too long…</h2>
+              <p>
+                Most couples wait too long before they seek the help that they
+                need. If you are browsing this page, chances are that it’s time for
+                you to take some action.
+              </p>
+            </div>
+            <div className="w-full">
+              <h2 className="text-3xl">Imagine how you and your partner would feel
+                if you:</h2>
+              <p>
+                <ul className="list-disc list-outside mx-10 my-6 text-sm">
+                  <li>Felt a sense of trust and ease with each other
+                  </li>
+                    <li>Had confidence and skills to talk about anything without
+                      fighting</li>
+                      <li>Enjoyed a more passionate sex life</li>
+                      <li>Knew how to support each other to be the best version of
+                        yourselves</li>
+                </ul>
+               
+              </p>
+            </div>
+<div class="aspect-w-16 aspect-h-9 mx-auto">
+  <div class="w-full" style={{height:'400px'}}>
+    <iframe class=" w-full h-full" src="https://www.youtube.com/embed/Ap1TxCYZCHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</div>
+
+
+          <div className="text-center md:text-left mt-8">
             <BookingButton />
           </div>
         </div>
-      </section>
+        <div className="w-1/3"></div>
+      </div>
+    </div>
+  </section>
     </Layout>
   )
 }
