1754db0894b10c9a18fd5b92e0c44caed82c3e20
hanif865 <99798066+hanif865@users.noreply.github.com>
2023-12-03 17:13:08 +0600

    Update index.en.js

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 //import { graphql, useStaticQuery } from "gatsby"
 //import Img from "gatsby-image"
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { BookingButton } from "../components/buttons"
 
 const Index = props => {
