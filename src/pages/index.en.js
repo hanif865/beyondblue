4222f1e6134d669b6772f92b3b561d9f66cda94f
hanif865 <99798066+hanif865@users.noreply.github.com>
2023-12-06 19:58:28 +0600

    Update index.en.js

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -34,7 +34,7 @@ const Index = props => {
       <div className="lg:flex">
         <div className="lg:w-2/3">
           <div className="lg:flex items-center">
-            <div className="lg:w-3/4">
+            <div className="lg:w-full">
               <div className="max-w-4xl mx-auto">
                 <h1 className="text-4xl lg:text-5xl text-center lg:text-left font-light">
                   Psychotherapy for testing
