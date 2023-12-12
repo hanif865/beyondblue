56e21a33965e08ca0989affeb502cb1d2b7ce44b
hanif865 <99798066+hanif865@users.noreply.github.com>
2023-12-12 22:19:57 +0600

    Update index.ja.js

--- src/pages/index.ja.js
+++ src/pages/index.ja.js
@@ -101,7 +101,7 @@ const Index = props => {
                 <h2>クライアント様のご感想</h2>
               </div>
               <div className=" py-3">
-                <div className="lg:w-full mb-6 bg-white p-4 ">
+                <div className="lg:w-full mb-6 bg-white p-4 rounded">
                   <p className="mb-0 text-md text-gray-900 text-justify">
                     <FontAwesomeIcon
                       icon="quote-left"
@@ -116,7 +116,7 @@ const Index = props => {
                     />
                   </p>
                 </div>
-                <div className="lg:w-full bg-white p-4">
+                <div className="lg:w-full bg-white p-4 rounded">
                   <p className="mb-0 text-md text-gray-900 text-justify">
                     <FontAwesomeIcon
                       icon="quote-left"
