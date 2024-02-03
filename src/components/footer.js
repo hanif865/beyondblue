45aa64411f371497d39dc00ddec50e5cb4263b8d
hanif865 <99798066+hanif865@users.noreply.github.com>
2024-02-03 06:46:05 +0600

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -73,16 +73,6 @@ export default () => {
           </div>
         </div>
         <hr />
-                  <script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4842097,4,6,200,40,00011111']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4842097&101" alt="website statistics" border="0"></a></noscript>
         <div className="py-6 text-center lg:text-left">
           © {new Date().getFullYear()}@beyondbleu.com. All rights reserved.      
         </div>
