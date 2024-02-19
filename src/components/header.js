254d72eceb3e95cce6dee9aedd09781192ee2c9c
hanif865 <99798066+hanif865@users.noreply.github.com>
2024-02-19 20:14:53 +0600

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react"
+import React, { useContext, useState, useEffect  } from "react"
 import AppContext from "../contexts/app_context"
 import { Link as GatsbyLink } from "gatsby"
 import Link from "./app_link"
