a12e4294d8af0bc3207f3dbe1f55ce6c3c63f5ff
hanif865 <99798066+hanif865@users.noreply.github.com>
2024-02-19 20:29:39 +0600

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -90,15 +90,14 @@ const SEO = ({ description, lang, meta, title }) => {
         href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&amp;family=Noto+Sans+JP:wght@300;400;700&amp;display=swap"
         rel="stylesheet"
       />
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-H7KVYDMBLJ"></script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-C9MQ6CT677"></script>
       <script>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-H7KVYDMBLJ');
-        `}
-      </script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-C9MQ6CT677');
+</script>
     </Helmet>
   )
 }
